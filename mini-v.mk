3455e4dafc9973175d993c21a5154373ca74c06b
viiniu <11e26db@gmail.com>
2026-08-12 13:54:47 +0900

    skel added

--- mini-v.mk
+++ mini-v.mk
@@ -4,3 +4,6 @@ PRODUCT_PACKAGES += \
 	MiniVChatPermissions \
 	ai_daemon \
 	ai_daemon_cli
+# FastRPC cDSP 스켈레톤
+PRODUCT_COPY_FILES += \
+	vendor/mini-v/ai/prebuilts/npu/dsp/libhtp_ops_skel.so:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/libhtp_ops_skel.so
